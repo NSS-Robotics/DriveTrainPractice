a42aab8d461bd57084cf472c0a705e3bb0a71a54
drinkliquidice <95946444+drinkliquidice@users.noreply.github.com>
2023-12-11 15:59:21 -0500

    started first drive train

--- test/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ test/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,7 +9,10 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup; //Importing MotorControllerGroup
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 /*
 The other imports are going to be CANSparkMax importS
 You see the exact imports on the API
@@ -17,13 +20,24 @@ We will go over Third-Party Vendor imports on Wednesday but you can just do your
 */
 
 public class DriveSubsystem extends SubsystemBase {
+  private static WPI_VictorSPX motor = new WPI_VictorSPX(0);
+  private static WPI_VictorSPX motor1 = new WPI_VictorSPX(1);
+  MotorControllerGroup leftdrive = new MotorControllerGroup(motor, motor1);
+
+  private static WPI_VictorSPX motor2 = new WPI_VictorSPX(2);
+  private static WPI_VictorSPX motor3 = new WPI_VictorSPX(3);
+  MotorControllerGroup rightdrive = new MotorControllerGroup(motor2, motor3);
+
+  private final DifferentialDrive drive = new DifferentialDrive(leftdrive, rightdrive);
 
   public DriveSubsystem() 
   {
-    /*
-     Example of CANSparkMax Initialization
-     CANSparkMax motor = new CANSparkMax(1, KBrushed);
-     */
+    this.leftdrive.setInverted(true);
+  }
+
+  public void setMaxOutput(double output)
+  {
+    this.drive.setMaxOutput(1);
   }
 
 
